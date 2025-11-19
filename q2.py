ea35bfdfd05bdb9100da349ca9ccfee692223248
akamranli24 <akamranli24@ku.edu.tr>
2025-11-19 17:28:20 +0300

    aaaaaa

--- q2.py
+++ q2.py
@@ -5,17 +5,12 @@ def remove_adjacent_duplicates(s):
     while True:
         new_s = ""
         i = 0
-
-        while i < len(s):
-            
+        while i < len(s):            
             if i + 1 < len(s) and s[i] == s[i+1]:
                 i += 2
             else:
                 new_s += s[i]
-                i += 1
-        
-        
+                i += 1               
         if new_s == s:
-            return s
-        
+            return s        
         s = new_s
